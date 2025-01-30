80f864e86dcfad9b83b34ff00065eda2a9130a19
wesleypds <wlypereiraa@gmail.com>
2025-01-30 12:21:58 -0300

    🚧 wip: add entidade vendedor

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,24 @@
 package app;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import model.entities.Departamento;
+import model.entities.Vendedor;
+
 public class App {
     public static void main(String[] args) {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Departamento dep = new Departamento(1, "Cadernos");
+        Vendedor vendedor;
+
+        try {
+            vendedor = new Vendedor(1, "Wesley Pereira", "wesley@email.com", sdf.parse("22/05/1997"), 2500.00, dep);
+            System.err.println(vendedor);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
     }
 }
